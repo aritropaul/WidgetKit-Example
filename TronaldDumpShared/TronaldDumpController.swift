b97397f1f22075e534d51be7dc74b521cdd22b3e
Aritro Paul <aritro.paul61@gmail.com>
2020-06-27 18:48:38 +0530

    Widgets

--- TronaldDumpShared/TronaldDumpController.swift
+++ TronaldDumpShared/TronaldDumpController.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Quote : Codable {
+    var value: String?
+}
+
+
+class QuoteController {
+    
+    let url = URL(string: "https://tronalddump.io/random/quote")!
+    static let shared = QuoteController()
+    
+    func getQuote(completion : @escaping(Quote)->Void) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            let quote = try! JSONDecoder().decode(Quote.self, from: data)
+            completion(quote)
+        }
+        
+        task.resume()
+    }
+    
+}
--- TronaldDumpWidget/TronaldDumpWidget.swift
+++ TronaldDumpWidget/TronaldDumpWidget.swift
@@ -7,58 +7,64 @@
 
 import WidgetKit
 import SwiftUI
-import Intents
 
-struct Provider: IntentTimelineProvider {
-    public func snapshot(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+@main
+struct QuoteWidget: Widget {
+    var body: some WidgetConfiguration {
+        StaticConfiguration(
+            kind: "QuoteWidget",
+            provider: Provider(),
+            placeholder: Text("Cool Stuff by Dolan Trum"),
+            content: { entry in
+                WidgetEntryView(entry: entry)
+            }
+        )
+        .configurationDisplayName("Quote Widget")
+        .description("Shows the dumbest stuff said by Donald Trump")
+    }
+}
+
+struct WidgetEntryView: View {
+    var entry: QuoteEntry
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(entry.quote.value ?? "")
+                .italic()
+                .font(.caption)
+            Text("- Donald Trump")
+                .foregroundColor(.secondary)
+                .font(.footnote)
+        }
+        .padding()
+    }
+}
+
+struct QuoteEntry: TimelineEntry {
+    var date: Date
+    var quote: Quote
+}
+
+
+struct Provider: TimelineProvider {
+    var loader = QuoteController()
+
+    func snapshot(with context: Context,
+                  completion: @escaping (QuoteEntry) -> ()) {
+        let entry = QuoteEntry(date: Date(), quote: Quote(value: "President of the US of A"))
         completion(entry)
     }
 
-    public func timeline(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
+    func timeline(with context: Context,
+                  completion: @escaping (Timeline<QuoteEntry>) -> ()) {
+        
+        let components = DateComponents(minute: 10)
+        let futureDate = Calendar.current.date(byAdding: components, to: Date())!
+        
+        loader.getQuote { (quote) in
+            let entry = QuoteEntry(date: Date(), quote: quote)
+            let timeline = Timeline(entries: [entry], policy: .after(futureDate))
+            completion(timeline)
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    public let date: Date
-    public let configuration: ConfigurationIntent
-}
-
-struct PlaceholderView : View {
-    var body: some View {
-        Text("Placeholder View")
-    }
-}
-
-struct TronaldDumpWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
-
-@main
-struct TronaldDumpWidget: Widget {
-    private let kind: String = "TronaldDumpWidget"
-
-    public var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(), placeholder: PlaceholderView()) { entry in
-            TronaldDumpWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
     }
 }
